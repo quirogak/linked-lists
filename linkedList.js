1f432db9331b1c921612dec1c0a0e42fee978ee5
quirogak <quirogakill@gmail.com>
2023-01-24 14:14:53 -0500

    Added the basic structure of Node and LinkedList.

---
+++ linkedList.js
@@ -0,0 +1,25 @@
+
+const Node = (data,next) => {return {data,next}}
+
+const LinkedList = () => {
+    return {
+        head:null,
+        size:0 
+    }
+
+}
+
+const LinkedListExample = LinkedList()
+
+console.log(LinkedListExample)
+
+
+/*
+
+const n1 = Node(100)
+const n2 = Node(200)
+n1.next = n2
+
+
+console.log(n1)
+*/
