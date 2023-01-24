98a0667038c34b7e1c4476202f4712b380abaeb3
quirogak <quirogakill@gmail.com>
2023-01-24 16:43:14 -0500

    Added a prepend and tail functions.
    I'm still working on the append function, but thanks to triying different algorithms, i achieved the logic for the tail function.

--- linkedList.js
+++ linkedList.js
@@ -1,15 +1,57 @@
 
-const Node = (data,next) => {return {data,next}}
+const Node = (data,next) => {
+
+   if (data == undefined){
+    data = null
+   }
+   if (next == undefined){
+    next = null
+   }
+
+    return {data,next}
+}
+
 
 const LinkedList = () => {
     return {
         head:null,
-        size:0 
+        size:0,
+        append: function(data){
+
+            newTail = Node(data)
+                                                       
+            this.head.next.next = newTail 
+
+            //on this algorithm, i need to add next "size" times to change append a new tail.
+
+            this.size++
+        },
+        prepend : function (data) {
+            currentHead = this.head
+            this.head = Node(data,currentHead)  //we save the current value of head, and then, we put the new node as head, and the saved value of head, will be the next node of the new one.
+            this.size++
+        },
+        tail: function(){
+            let currentNode = this.head
+            let nextNode = currentNode.next
+
+            while(nextNode != null ){
+                //while nextNode isn't null, we keep advancing through the linked list, by redefining the currentNode to the "nextNode", and nextNode is the next Node of this redefined variable.
+                currentNode = nextNode
+                nextNode = currentNode.next
+            }
+            return console.log(currentNode)
+        },
     }
 
 }
 
 const LinkedListExample = LinkedList()
+
+LinkedListExample.prepend(100)
+LinkedListExample.prepend(200)
+LinkedListExample.append(300)
+LinkedListExample.tail()
 
 console.log(LinkedListExample)
 
