e27d77255b733d617ceae0fc042c275e21e97472
quirogak <quirogakill@gmail.com>
2023-01-25 12:36:52 -0500

    Refactored the linkedList, finished append().
    I had to create the tail as a property in order to have real access to it and be able to append() new nodes.

--- linkedList.js
+++ linkedList.js
@@ -15,53 +15,37 @@ const Node = (data,next) => {
 const LinkedList = () => {
     return {
         head:null,
+        tail:null,
         size:0,
         append: function(data){
 
-            newTail = Node(data)
-                                                       
-            this.head.next.next = newTail 
-
-            //on this algorithm, i need to add next "size" times to change append a new tail.
-
+            this.tail.next = Node(data)  //we append the node to the last.next node.
+            this.tail = this.tail.next  //we change the tail value to the new node.
             this.size++
         },
         prepend : function (data) {
             currentHead = this.head
-            this.head = Node(data,currentHead)  //we save the current value of head, and then, we put the new node as head, and the saved value of head, will be the next node of the new one.
-            this.size++
-        },
-        tail: function(){
-            let currentNode = this.head
-            let nextNode = currentNode.next
-
-            while(nextNode != null ){
-                //while nextNode isn't null, we keep advancing through the linked list, by redefining the currentNode to the "nextNode", and nextNode is the next Node of this redefined variable.
-                currentNode = nextNode
-                nextNode = currentNode.next
+            this.head = Node(data,currentHead) //we save the current value of head, and then, we put the new node as head, and the saved value of head, will be the next node of the new one.
+            
+            if(this.head.next == null){ //if we prepend a new node, IE for the first time, the next node will be null, so this new head will be also the tail. So if we keep pushing new nodes using prepend, the tail will always be correct.
+               this.tail = this.head
             }
-            return console.log(currentNode)
-        },
+
+            this.size++
+
+        }
     }
 
 }
+
+
 
 const LinkedListExample = LinkedList()
 
 LinkedListExample.prepend(100)
 LinkedListExample.prepend(200)
 LinkedListExample.append(300)
-LinkedListExample.tail()
+LinkedListExample.tail
 
 console.log(LinkedListExample)
 
-
-/*
-
-const n1 = Node(100)
-const n2 = Node(200)
-n1.next = n2
-
-
-console.log(n1)
-*/
